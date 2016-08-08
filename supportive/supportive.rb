2951b9caabedff570fa917818cf7d23faae53163
James Wilton <jwilton@MacBook-Pro-3.local>
2016-08-08 13:52:00 -0500

    Add parens

--- supportive/supportive.rb
+++ supportive/supportive.rb
@@ -76,7 +76,7 @@ class BlagPost
     return '' unless comments_allowed?
     return '' unless comments.length > 0
 
-    ordinal = case comments.length + 1 % 10
+    ordinal = case (comments.length + 1) % 10
       when 1 then "st"
       when 2 then "nd"
       when 3 then "rd"
