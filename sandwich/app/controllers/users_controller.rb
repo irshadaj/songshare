da86f15f2cd2349a8d977c3be0279e35e319fb54
Shaan Sapra <ssapra@enova.com>
2016-09-06 17:15:26 -0500

    Remove unnecessary code in user controller and add tests for index/show actions

--- sandwich/app/controllers/users_controller.rb
+++ sandwich/app/controllers/users_controller.rb
@@ -1,20 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :retrieve_users, only: [:index]
-  before_action :retrieve_current_user, only: [:edit, :update]
-
-  def update
-    if @user.update(profile_params)
-      flash[:notice] = "Profile updated."
-      redirect_to :show
-    else
-      render :edit
-    end
-  end
 
   def show
-    if params[:id] && params[:id] != current_user.id
-      @user = User.visible.find(params[:id])
+    if params[:id] && params[:id].to_i != current_user.id
+      @user = User.visible.find_by_id(params[:id])
+      redirect_to root_url if @user.nil?
     else
       @user = current_user
     end
@@ -22,17 +13,7 @@ class UsersController < ApplicationController
 
   private
 
-  def retrieve_current_user
-    @user = current_user
-  end
-
   def retrieve_users
     @users = User.visible.order(:last_name, :first_name)
-  end
-
-  def profile_params
-    params.require(:user).permit(:last_name, :first_name, :phone, :city, :state,
-                                 :zip, :about, :profile_visible,
-                                 :contact_visible)
   end
 end
